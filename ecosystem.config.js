a67267eacda14b172e0ff625f0d586648317c7fa
Shivam Rana <shivamrana.cpp@gmail.com>
2025-05-24 14:28:55 +0530

    fix: update Supabase anon key in ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -13,14 +13,14 @@ module.exports = {
         PORT: 9876,
         ALLOWED_ORIGINS: 'http://localhost:5789',
         SUPABASE_URL: 'https://irxwklpjxecoettwfeiq.supabase.co',
-        SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlyeHdrbHBqeGVjb2V0dHdmZWlxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDI5NzMwNzgsImV4cCI6MjAxODU0OTA3OH0.RYyD6eeGQOm8IRgZIkN6j_6GWFHDOAQp_cAHHkEBhB4'
+        SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlyeHdrbHBqeGVjb2V0dHdmZWlxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc5MTgyMjQsImV4cCI6MjA2MzQ5NDIyNH0.Gtw5qCBBQ8EP0Fakj9caeI3K4WGzKje3lHAOAIlSekI'
       },
       env_production: {
         NODE_ENV: 'production',
         PORT: 9876,
         ALLOWED_ORIGINS: 'http://116.202.210.102:5789',
         SUPABASE_URL: 'https://irxwklpjxecoettwfeiq.supabase.co',
-        SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlyeHdrbHBqeGVjb2V0dHdmZWlxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDI5NzMwNzgsImV4cCI6MjAxODU0OTA3OH0.RYyD6eeGQOm8IRgZIkN6j_6GWFHDOAQp_cAHHkEBhB4'
+        SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlyeHdrbHBqeGVjb2V0dHdmZWlxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc5MTgyMjQsImV4cCI6MjA2MzQ5NDIyNH0.Gtw5qCBBQ8EP0Fakj9caeI3K4WGzKje3lHAOAIlSekI'
       }
     },
     {
